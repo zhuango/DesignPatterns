ad7e9f2cb0b06cb18bf32342aff1c5a9d761c8a9
LiuZhuang <og192liu@gmail.com>
2014-05-07 22:21:37 +0800

    add Contant class
    add Contant class

---
+++ Interpreter/Constant.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interpreter
+{
+    public class Constant : IBooleanExp
+    {
+        private Boolean booleanValue;
+
+        public Constant(Boolean value)
+        {
+            this.booleanValue = value;
+        }
+
+        public bool Evaluate(Context context)
+        {
+            return this.booleanValue;
+        }
+
+        public IBooleanExp Replace(string statement, IBooleanExp exp)
+        {
+            return new Constant(this.booleanValue);
+        }
+
+        public IBooleanExp Copy()
+        {
+            return new Constant(this.booleanValue);
+        }
+    }
+}
